d07f3a97357c74db53de689b9e5a461926e7f409
evan-chernicky <echernicky@gmail.com>
2023-06-21 12:53:00 -0400

    got stars working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react'
+import React, {useEffect, useMemo, useRef, useState} from 'react'
 import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import { motion, useScroll, useMotionValueEvent } from "framer-motion"
@@ -31,54 +31,58 @@ export default function Home() {
     })
 
 
-    const TheSketch = useMemo(() => {
+      const drawStars = useMemo(() => {
+        const stars: Star[] = [];
       
-      const stars: Star[] = [];
-      let bg: any
-
-      class Star {
-        p5: p5Types;
-        x: number;
-        y: number;
-        size: number;
-        t: number;
-      
-        constructor(p5: p5Types) {
-          this.p5 = p5
-          this.x = p5.random(p5.width);
-          this.y = p5.random(p5.height);
-          this.size = p5.random(0.25, 3);
-          this.t = p5.random(p5.TAU);
-        }
+        let bg: any
+        let canvasCounter = 0;
+  
+        class Star {
+          p5: p5Types;
+          x: number;
+          y: number;
+          size: number;
+          t: number;
         
-        draw() {
-          this.t += 0.1;
-          var scale = this.size + this.p5.sin(this.t) * 1;
-          this.p5.noStroke();
-          this.p5.ellipse(this.x, this.y, scale, scale);
+          constructor(p5: p5Types) {
+            this.p5 = p5
+            this.x = p5.random(p5.width);
+            this.y = p5.random(p5.height);
+            this.size = p5.random(0.25, 3);
+            this.t = p5.random(p5.TAU);
+          }
+          
+          draw() {
+            this.t += 0.03;
+            var scale = this.size + this.p5.sin(this.t) * 1;
+            this.p5.noStroke();
+            this.p5.ellipse(this.x, this.y, scale, scale);
+          }
         }
-      }
-
-      const setup = (p5: p5Types, canvasParentRef: Element) => {
-        bg = p5.loadImage(NightSky.src);
-        p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
   
-        for (var i = 0; i < 200; i++) {
-          stars[i] = new Star(p5);
+        const setup = (p5: p5Types, canvasParentRef: Element) => {
+          canvasCounter = canvasCounter + 1;
+          if (canvasCounter > 1) return
+          bg = p5.loadImage(NightSky.src);
+          p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  
+          for (var i = 0; i < 200; i++) {
+            stars[i] = new Star(p5);
+          }
         }
-      }
+    
+      const draw = (p5: p5Types) => {
+        if (!bg) return; // Check if the background image is loaded before drawing
+        p5.background(bg);
   
-    const draw = (p5: p5Types) => {
-      p5.background(bg);
-	
-      for (var i = 0; i < stars.length; i++) {
-        stars[i].draw();
-      }
-      
-    };
+        for (var i = 0; i < stars.length; i++) {
+          stars[i].draw();
+        }
+      };
 
       return <Sketch setup={setup} draw={draw}/>
-    },[])
+
+      },[])
 
 
 
@@ -89,8 +93,8 @@ export default function Home() {
       <Intro />
         <Header isVisible={isVisible} fullPageRef={fullPageRef.current} />
             <motion.div ref={ref} className="relative min-h-[375vh] w-full h-full">
-                <div  className={`fixed w-full h-full`} style={{transform: `translateY(-${starPosition * 1.5}px)`}}>
-                {TheSketch}
+                <div  className={`thing fixed w-full h-full`} style={{transform: `translateY(-${starPosition * 1.5}px)`}}>
+                {drawStars}
                 </div>
                 <div className="text-white absolute bottom-0">
                     <Hero />
